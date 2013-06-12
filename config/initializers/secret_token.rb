ce40604a91e6b7b882a7ddee376e89c5127989d3
c.v <csanta.v@gmail.com>
2013-06-12 16:25:53 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Frisbee::Application.config.secret_token = '291b75f902d5fe1f77808c9a20f357b2c6f32024209a251aa9f9e1c4087e00f0852e55fc3da8d07ae6b4136084dc4a71c484d2d74716bd54b51fe4f93c863335'
